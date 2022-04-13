4e74c2ed4492ed0717cc7b4babf2b89057c31261
younahshin-dev <roaving@gmail.com>
2022-04-13 17:43:52 +0900

    useEffect CleanUp

--- section10_effect_reduce_contextAPI/src/components/Login/Login.js
+++ section10_effect_reduce_contextAPI/src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('check form');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      clearTimeout(identifier);
+      console.log('clean up');
+    };
   }, [enteredEmail, enteredPassword]);
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
