c935884204abdb210884257fb38e112e551f5e96
younahshin-dev <roaving@gmail.com>
2022-04-13 17:06:15 +0900

    useEffect 종속성

--- section10_effect_reduce_contextAPI/src/App.js
+++ section10_effect_reduce_contextAPI/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,21 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInformation = localStorage.getItem('isLoggedIn');
+    if (storedUserLoggedInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []); //의존성 배열에 담은 내용이 없기 때문에 한번 실행될떼한 실행되고 그후에는 의존성배열에 담긴것도 없고 -> 변경을 감지 X 최초 한번만 실행되은 effect
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1');
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
